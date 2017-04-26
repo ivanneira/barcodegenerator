1fdff5068e7738495d2e44479d6037fc2af08fa7
ivanneira <ien1983@gmail.com>
2017-04-26 10:02:25 -0300

    primeros tests

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ if(isset($_GET['code'])) {
 
 } else {
     //echo "error en la petición";
-
-    $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
-    echo $generator->getBarcode('081231723897', $generator::TYPE_CODE_128);
+    $generator = new \Picqer\Barcode\BarcodeGeneratorPNG();
+    echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('081231723897', $generator::TYPE_CODE_128)) . '">';
 }
