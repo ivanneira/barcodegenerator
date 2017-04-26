6fcaf9f268c77730ac522a0d53a2933f05941937
ivanneira <ien1983@gmail.com>
2017-04-26 10:13:43 -0300

    cambio a barcode bakery

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ if(isset($_GET['code'])) {
 
 
 } else {
-    //echo "error en la petición";
-    $generator = new barcodegenerator\src\BarcodeGeneratorPNG();
-    echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('081231723897', $generator::TYPE_CODE_128)) . '">';
+    echo "error en la petición";
+
 }
