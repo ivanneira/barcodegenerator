b926eb98775de99d4dc0d4834cea2ea872b7d371
ivanneira <ien1983@gmail.com>
2017-04-26 10:46:07 -0300

    cambio a barcode bakery

--- index.php
+++ index.php
@@ -60,3 +60,25 @@ function generateBarcode($text){
 
 
 }
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <script
+        src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous"></script>
+</head>
+<body>
+
+<img id="test"></img>
+</body>
+</html>
