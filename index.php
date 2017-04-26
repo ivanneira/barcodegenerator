8cc9c3b14c74261cd5315c18648d9d8190bd205a
ivanneira <ien1983@gmail.com>
2017-04-26 11:38:40 -0300

    cambio a barcode generator https://www.phpclasses.org/package/2176-PHP-Web-interface-to-generate-barcode-images.html#download

--- index.php
+++ index.php
@@ -24,5 +24,5 @@
 
 <script>
 
-    $.post("http://10.64.65.200:84/barcodegenerator/test.php").done(function(data){$("#test").html(data)});
+    $.post("http://10.64.65.200:84/barcodegenerator/test.php").done(function(data){$("#test").attr('src',data)});
 </script>
